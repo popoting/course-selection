874b0dc354afbf335d46bf4e07c96e9332beaf69
你的名字 <email@xxx.xxx>
2017-12-12 23:58:50 +0800

    Reflash1

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,5 @@ class Student < ApplicationRecord
   has_many :teachers, :through => :teacher_relationship, :source => :teacher
   has_many :courses, :through => :course_relationship, :source => :course
   has_many :teacher_relationships
-  has_many :course_relationships
+  has_many :course_relationship
 end
